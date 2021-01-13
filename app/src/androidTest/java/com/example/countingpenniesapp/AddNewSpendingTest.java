eedfea891f8c55204609b0fc061acebbefee67a6
DariaBezuhla <daria.freebird20@gmail.com>
2021-01-13 21:03:32 +0100

    edited AddNewSpendingTest class

--- app/src/androidTest/java/com/example/countingpenniesapp/AddNewSpendingTest.java
+++ app/src/androidTest/java/com/example/countingpenniesapp/AddNewSpendingTest.java
@@ -31,7 +31,7 @@ public class AddNewSpendingTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void test_1_insert_Spending_in_recycler_view() {
+    public void test_insert_Expense_spending_in_recycler_view() {
         ViewInteraction floatingActionButton = onView(
                 allOf(withId(R.id.fab),
                         childAtPosition(
@@ -81,6 +81,69 @@ public class AddNewSpendingTest {
         textView1.check(matches(withText("4.60")));
 
     }
+
+    @Test
+    public void test_insert_income_spending() {
+        ViewInteraction floatingActionButton2 = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        floatingActionButton2.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.spendingAmountView),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.design_bottom_sheet),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("-2000.30"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.spendingNameView),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.design_bottom_sheet),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("monthly salary"), closeSoftKeyboard());
+
+
+        //TODO: the part where espresso does not want to take the root of the spinner item
+//        ViewInteraction appCompatSpinner = onView(
+//                allOf(withId(R.id.categories_spinner),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(R.id.design_bottom_sheet),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        appCompatSpinner.perform(click());
+//
+//        DataInteraction appCompatCheckedTextView = onData(anything())
+//                .inAdapterView(childAtPosition(
+//                        withSpinnerText(containsString("Income")),
+//                        0))
+//                .atPosition(1);
+//        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.addingValueButton), withText("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.design_bottom_sheet),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
+    }
+
 
 
     private static Matcher<View> childAtPosition(
